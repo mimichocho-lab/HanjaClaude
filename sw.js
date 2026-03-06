af0b058311b7c55cd08ace2aaab4eca8d37181cf
hapines <hapines@users.noreply.github.com>
2026-03-06 03:12:24 +0000

    deploy: b7a71c8ee1e198c85b9f7de2bfc95c144c41c186

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "hanjaclaude-v1";
+const STATIC_ASSETS = [
+  "/",
+  "/options",
+  "/play",
+  "/wrong",
+  "/data/hanjab.csv",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
